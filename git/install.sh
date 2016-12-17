57285f0b6bb51c39b4fe95a65f4578b911337bf4
Guillaume Frichet <guillaume.frichet@gmail.com>
2016-12-17 16:19:42 +0100

    +

--- git/install.sh
+++ git/install.sh
@@ -1,12 +1,10 @@
 case "$OS" in
   android)
     has git || apt install git
-    if [[ ! -e "$HOME/.gitconfig.local" ]]
-    then > "$HOME/.gitconfig.local" \
-        template "$BOOTSTRAP/git/.gitconfig.local.tpl" \
-        "GIT_NAME:What is your github full name?" \
-        "GIT_USERNAME:What is your github username?" \
-        "GIT_EMAIL:What is your github email?"
-    fi
+    template "$BOOTSTRAP/git/.gitconfig.local.tpl" \
+      "$HOME/.gitconfig.local" \
+      "GIT_NAME:What is your github full name?" \
+      "GIT_USERNAME:What is your github username?" \
+      "GIT_EMAIL:What is your github email?"
     ;;
 esac
