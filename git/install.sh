50664cdaa4e232d79e5fcc2c2fa7037692714229
Guillaume Frichet <guillaume.frichet@gmail.com>
2016-12-19 00:04:11 +0100

    fixme:prompt

--- git/install.sh
+++ git/install.sh
@@ -1,4 +1,6 @@
-template "$BOOTSTRAP/git/.gitconfig.local.tpl" "$HOME/.gitconfig.local" \
-  "GIT_AUTHOR_NAME:What is your github full name?" \
-  "GIT_AUTHOR_USERNAME:What is your github username?" \
-  "GIT_AUTHOR_EMAIL:What is your github email?"
+_post_install() {
+  template "$BOOTSTRAP/git/.gitconfig.local.tpl" "$HOME/.gitconfig.local" \
+    "GIT_AUTHOR_NAME:What is your github full name?" \
+    "GIT_AUTHOR_USERNAME:What is your github username?" \
+    "GIT_AUTHOR_EMAIL:What is your github email?"
+}
