35ecacaebcd1c41ddfa67c47d80c5929acd5e8c3
Guillaume Frichet <guillaume.frichet@gmail.com>
2016-12-18 18:25:42 +0100

    +

--- bash/install.sh
+++ bash/install.sh
@@ -1,15 +1,4 @@
-bash_pkg="bash bash-completion tree"
-
-case "$OS" in
-  android) apt install -qqy $bash_pkg ;;
-  *linux)
-    if has apt-get 2>/dev/null
-    then apt-get install -qqy $bash_pkg
-    elif has apt 2>/dev/null
-    then apt install -qqy $bash_pkg
-    fi
-    ;;
-esac
+packages="bash bash-completion tree"
 
 directory present $HOME/bin
 
