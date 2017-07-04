0cc51f77873db46819c67b1bc128d62707651b06
LEI <lei@ip-172-31-3-234.eu-west-2.compute.internal>
2017-07-04 13:57:37 +0000

    line

---
+++ cmd/line.go
@@ -0,0 +1,73 @@
+// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	// "fmt"
+	"os"
+	"path"
+	// "path/filepath"
+	// "strings"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+)
+
+// lineCmd represents the line command
+var lineCmd = &cobra.Command{
+	Use:   "line",
+	Short: "Add or remove a line in file",
+	Long:  ``,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return doLine(viper.GetStringMapString("line"))
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(lineCmd)
+
+	// lineCmd.Flags().BoolVarP(&Bool, "bool", "b", Bool, "Example boolean")
+}
+
+func doLine(in map[string]string) error {
+	for dst, str := range in {
+		dst = os.ExpandEnv(dst)
+		if !path.IsAbs(dst) {
+			dst = path.Join(Target, dst)
+		}
+		dst = path.Clean(dst)
+		// _, err := createDir(dst)
+		// if err != nil {
+		// 	return err
+		// }
+		err := lineInFile(dst, str)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func lineInFile(file string, line string) error {
+	str := `if ! test -f "` + file + `" || ! grep -Fxq '` + line + `' "` + file + `"; then echo '` + line + `' >> "` + file + `"; fi;`
+	err := executeCmd(Shell, []string{"-c", str}...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
