3764b9eb4ddb3a276bf59031e497376dc43d6137
Guillaume Frichet <guillaume.frichet@gmail.com>
2018-07-13 21:15:21 +0200

    dot

--- scripts/test.sh
+++ scripts/test.sh
@@ -18,7 +18,7 @@ tail_bashrc="$(tail -n1 ~/.bashrc)"
 yes | run dot --packages # --verbose
 run tmux -2 -u new-session -n test "vim -E -s -u $HOME/.vimrc +Install +qall; exit"
 for f in "$HOME"/.gitconfig; do run test -f "$f"; done
-for d in "$HOME"/{.tmux/plugins/tpm,.vim/plugged}; do run test -s "$d"; done
+for d in "$HOME"/{.tmux/plugins/tpm,.vim/pack/config}; do run test -s "$d"; done
 [[ "$(tail -n1 ~/.bashrc)" != "$tail_bashrc" ]] || exit 1
 # yes | run dot remove -c $DOT/.dotrc.yml
 # yes | run dot rm --https --empty;
