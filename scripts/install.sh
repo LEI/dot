664038f535f6fe058732b411e6ab810363c5d16d
Guillaume Frichet <guillaume.frichet@gmail.com>
2018-07-14 12:38:08 +0200

    0.0.3

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,23 +1,42 @@
 #!/bin/sh
 
-# https://raw.githubusercontent.com/LEI/dot/blob/master/scripts/install.sh | sh
+# https://raw.githubusercontent.com/LEI/dot/master/scripts/install.sh
+# curl -sSL https://dot.lei.sh | sh
+# wget -qO- https://dot.lei.sh | sh
 
 # https://github.com/rootbeersoup/get.darryl.sh/blob/master/scripts/dotfiles.sh
 # https://github.com/golang/dep/blob/master/install.sh
+# https://get.docker.com/
 
 set -e
 
 REPO="$REPO"
 
-if [ ! -d "$GOPATH/src/$REPO" ]; then
-  # git clone https://$REPO.git
-  # Use --recursive for .gitmodules
+has() {
+  hash "$1" 2> /dev/null
+}
 
-  go get "$REPO"
-fi
+do_install() {
+  if ! has go; then
+    echo "Go command is unavailable"
+    exit 1
+  fi
+  if [ ! -n "$GOPATH" ]; then
+    echo "GOPATH is not set"
+    exit 1
+  fi
+  if [ ! -d "$GOPATH/src/$REPO" ]; then
+    # git clone https://$REPO.git
+    # Use --recursive for .gitmodules
 
-if ! hash dot 2> /dev/null; then
-  go install "$REPO"
-fi
+    go get "$REPO"
+  fi
 
-dot --dry-run --verbose
+  if ! has dot; then
+    go install "$REPO"
+  fi
+
+  dot --dry-run --verbose
+}
+
+do_install
