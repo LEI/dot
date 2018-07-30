16d68fcf285278b5404b6251cb2ce5c36be4faa0
Guillaume Frichet <guillaume.frichet@gmail.com>
2018-07-30 09:57:27 +0200

    d

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,18 @@
 
 set -e
 
+if ! hash dep 2> /dev/null; then
+  curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
+fi
+
+if ! hash goreleaser 2> /dev/null; then
+  go get -d github.com/goreleaser/goreleaser && (
+    cd "$GOPATH/src/github.com/goreleaser/goreleaser"
+    dep ensure -vendor-only
+    make setup build
+  )
+fi
+
 goreleaser --rm-dist --snapshot
-
-docker-compose build
-
-docker-compose run test
 
 ls -la dist
