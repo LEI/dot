e7554f345b10fee7f570128734c0e6b028fb585b
Guillaume Frichet <guillaume.frichet@gmail.com>
2018-08-03 00:09:52 +0200

    s

--- scripts/setup-user.sh
+++ scripts/setup-user.sh
@@ -8,18 +8,9 @@ if [ -z "$USER" ]; then
   exit 1
 fi
 
-# Add /usr/local/bin to PATH while sudoing
-#sed -e 's#Defaults[[:blank:]]+secure_path = /sbin:/bin:/usr/sbin:/usr/bin#Defaults[[:blank:]]+secure_path = /sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin#' /etc/sudoers
-# Deb: Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-# sed -e '/secure_path/ s[=.*[&:/usr/local/bin[' /etc/sudoers
-# sed -r -e '/^\s*Defaults\s+secure_path/ s[=(.*)[=\1:/usr/local/bin[' /etc/sudoers
-
 # groupadd $GROUP
 # echo "%$GROUP ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/$GROUP
 # usermod -aG sudo $USER
-
-# # Allow members of group to execute any command without password
-echo "%$USER ALL=(ALL) NOPASSWD: ALL" >> "/etc/sudoers.d/$USER"
 
 #shopt -s extglob
 chmod 0440 /etc/sudoers.d/*
@@ -27,3 +18,14 @@ chmod 0440 /etc/sudoers.d/*
 # Create user: --password '' --groups sudo,...
 useradd --create-home --shell /bin/bash --user-group "$@"
 # echo "$USER:$USER" | chpasswd
+
+# Add /usr/local/bin to sudo PATH
+#sed -e 's#Defaults    secure_path = /sbin:/bin:/usr/sbin:/usr/bin#Defaults    secure_path = /sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin#' /etc/sudoers
+#sed -e 's#Defaults[[:blank:]]+secure_path = /sbin:/bin:/usr/sbin:/usr/bin#Defaults[[:blank:]]+secure_path = /sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin#' /etc/sudoers
+# Deb: Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+# sed -e '/secure_path/ s[=.*[&:/usr/local/bin[' /etc/sudoers
+# sed -r -e '/^\s*Defaults\s+secure_path/ s[=(.*)[=\1:/usr/local/bin[' /etc/sudoers
+echo 'Defaults secure_path="<default value>:/usr/local/bin"' >> "/etc/sudoers.d/$USER"
+
+# Allow user to execute any command without password
+echo "$USER ALL=(ALL) NOPASSWD: ALL" >> "/etc/sudoers.d/$USER"
