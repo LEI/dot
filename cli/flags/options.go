74848f7bcb2873497f652631fc484d1a25f8f55c
Guillaume Frichet <guillaume.frichet@gmail.com>
2018-08-08 22:29:38 +0200

    dr

--- cli/flags/options.go
+++ cli/flags/options.go
@@ -27,7 +27,7 @@ func (cmdOpts *Options) InstallFlags(flags *pflag.FlagSet) {
 	flags.StringVarP(&cmdOpts.Target, "target", "t", "", "Target directory")
 
 	flags.StringVar(&cmdOpts.ConfigDir, "config", cliconfig.Dir(), "Location of config file") // (s)
-	flags.BoolVarP(&cmdOpts.DryRun, "debug", "d", false, "Enable debug mode")
+	flags.BoolVarP(&cmdOpts.DryRun, "dry-run", "d", false, "Do not execute tasks")
 	flags.StringVarP(&cmdOpts.LogLevel, "log-level", "l", "info", `Set the logging level ("debug"|"info"|"warn"|"error"|"fatal")`)
 	flags.BoolVarP(&cmdOpts.Version, "version", "v", false, "Print version information and quit")
 
