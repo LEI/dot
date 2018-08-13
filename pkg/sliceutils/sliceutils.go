0e2a871ef0f7780c03ff4156ca45ad087c3b46b8
Guillaume Frichet <guillaume.frichet@gmail.com>
2018-08-13 18:30:57 +0200

    s

--- pkg/sliceutils/sliceutils.go
+++ pkg/sliceutils/sliceutils.go
@@ -1,38 +1,5 @@
 package sliceutils
 
-import (
-	"fmt"
-	"os"
-	"regexp"
-)
-
-// Matches ...
-func Matches(in []string, list []string) bool {
-	for _, pattern := range in {
-		negated := pattern[0] == '!'
-		if negated {
-			pattern = pattern[1:]
-		}
-		for _, str := range list {
-			matched, err := regexp.MatchString(pattern, str)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "%s: %s\n", pattern, err)
-				os.Exit(1)
-			}
-			if negated && matched {
-				return false
-			}
-			if matched {
-				return true
-			}
-		}
-		if negated {
-			return true
-		}
-	}
-	return false
-}
-
 // Contains check if a slice contains a given string
 func Contains(in []string, s string) bool {
 	for _, a := range in {
@@ -43,7 +10,7 @@ func Contains(in []string, s string) bool {
 	return false
 }
 
-// Intersects check if a slice contains at least one element from the other
+// Intersects check if a slice contains any element from the other one
 func Intersects(in []string, list []string) bool {
 	for _, a := range in {
 		if Contains(list, a) {
