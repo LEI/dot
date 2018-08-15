0d950dc5e5805260838a85536ece368dc9dab469
Guillaume Frichet <guillaume.frichet@gmail.com>
2018-08-15 17:20:00 +0200

    e

--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -15,10 +15,6 @@ main() {
 
   touch ~/.dotnc
 
-  run echo "$PATH"
-  run ls -la /usr/local/bin
-  run command -v dot
-
   tail_bashrc="$(tail -n1 ~/.bashrc)"
   run dot sync --verbose
   run dot install --verbose
