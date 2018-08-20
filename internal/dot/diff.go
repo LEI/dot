d5ce57d1b94231980667f8b9bce3f89c7f3a199a
Guillaume Frichet <guillaume.frichet@gmail.com>
2018-08-20 15:12:57 +0200

    d

--- internal/dot/diff.go
+++ internal/dot/diff.go
@@ -1,26 +1,33 @@
 package dot
 
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+)
+
 // "github.com/sergi/go-diff/diffmatchpatch"
 
-// func printDiff(s, content string) error {
-// 	// stdout, stderr, status := ExecCommand("")
-// 	diffCmd := exec.Command("diff", s, "-")
-// 	// --side-by-side --suppress-common-lines
-// 	stdin, err := diffCmd.StdinPipe()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer stdin.Close()
-// 	diffCmd.Stdout = os.Stdout
-// 	diffCmd.Stderr = os.Stderr
-// 	fmt.Println("START DIFF", s)
-// 	if err := diffCmd.Start(); err != nil {
-// 		return err
-// 	}
-// 	io.WriteString(stdin, content)
-// 	// fmt.Println("WAIT")
-// 	stdin.Close()
-// 	diffCmd.Wait()
-// 	fmt.Println("END DIFF", s)
-// 	return nil
-// }
+func printDiff(s, content string) error {
+	// stdout, stderr, status := ExecCommand("")
+	diffCmd := exec.Command("diff", s, "-")
+	// --side-by-side --suppress-common-lines
+	stdin, err := diffCmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+	defer stdin.Close()
+	diffCmd.Stdout = os.Stdout
+	diffCmd.Stderr = os.Stderr
+	fmt.Println("START DIFF", s)
+	if err := diffCmd.Start(); err != nil {
+		return err
+	}
+	io.WriteString(stdin, content)
+	// fmt.Println("WAIT")
+	stdin.Close()
+	diffCmd.Wait()
+	fmt.Println("END DIFF", s)
+	return nil
+}
