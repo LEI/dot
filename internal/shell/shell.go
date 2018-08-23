fff91b5fc25127eb8891b74ee743abf97e2cdeac
Guillaume <4112243+LEI@users.noreply.github.com>
2018-08-23 17:52:43 +0200

    w

--- internal/shell/shell.go
+++ internal/shell/shell.go
@@ -1,5 +1,30 @@
 package shell
 
+import (
+	"fmt"
+	"os"
+)
+
 var (
 	defaultShell = "sh" // command -v sh
 )
+
+// Key returns the env var name for the user's shell.
+func Key() string {
+	return "SHELL"
+}
+
+// Get returns the shell to use.
+func Get() string {
+	shell := os.Getenv(Key())
+	if shell == "" {
+		fmt.Fprintf(os.Stderr, "Fallback to default shell: %s", defaultShell)
+		shell = defaultShell
+	}
+	return shell
+}
+
+// GetShortcutString returns the variable to use in the native shell.
+func GetShortcutString() string {
+	return "$SHELL"
+}
