39d13dcce02634aef5e0e9e2f0a41eb3f3a30dca
LEI <guillaume.frichet@gmail.com>
2018-08-27 22:16:35 +0200

    toml

--- internal/conf/conf_test.go
+++ internal/conf/conf_test.go
@@ -1,6 +1,7 @@
 package conf
 
 import (
+	"path/filepath"
 	"reflect"
 	"testing"
 )
@@ -11,7 +12,27 @@ func TestRead(t *testing.T) {
 		in   []byte
 		out  map[string]interface{}
 	}{
-		{"test.json", []byte(`{"a": 1}`), map[string]interface{}{"a": float64(1)}},
+		{
+			"test.toml",
+			[]byte(`a = "b"`),
+			map[string]interface{}{
+				"a": "b",
+			},
+		},
+		{
+			"test.yaml",
+			[]byte(`a: b`),
+			map[string]interface{}{
+				"a": "b",
+			},
+		},
+		{
+			"test.json",
+			[]byte(`{"a": "b"}`),
+			map[string]interface{}{
+				"a": "b",
+			},
+		},
 	}
 	for _, tc := range testCases {
 		data, err := Read(tc.path, tc.in)
@@ -22,4 +43,19 @@ func TestRead(t *testing.T) {
 			t.Errorf("got %s; want %s", data, tc.out)
 		}
 	}
+	// JSON actually gets decoded as YAML w/o extension
+	for _, tc := range testCases {
+		tc.path = removeExt(tc.path)
+		data, err := Read(tc.path, tc.in)
+		if err != nil {
+			t.Fatalf("could not read file w/o extension: %s", err)
+		}
+		if !reflect.DeepEqual(data, tc.out) {
+			t.Errorf("got %s; want %s", data, tc.out)
+		}
+	}
+}
+
+func removeExt(path string) string {
+	return path[0 : len(path)-len(filepath.Ext(path))]
 }
