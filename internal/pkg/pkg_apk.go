60288468be579d01a70747f318932738aa60df0f
Guillaume <4112243+LEI@users.noreply.github.com>
2018-08-27 15:30:37 +0200

    ap

--- internal/pkg/pkg_apk.go
+++ internal/pkg/pkg_apk.go
@@ -1,10 +1,6 @@
 package pkg
 
-import (
-	"fmt"
-	"os"
-	"os/exec"
-)
+import "os/exec"
 
 // https://wiki.alpinelinux.org/wiki/Alpine_Linux_package_management
 var apk = &Pm{
@@ -16,27 +12,35 @@ var apk = &Pm{
 		"--no-cache",
 		"--no-progress",
 		"--quiet",
-		"--update",
+		// "--update",
+	},
+	InstallOpts: []string{
 		// "--upgrade",
 	},
 	DryRunOpts: []string{"--simulate"},
 	Has: func(m *Pm, pkgs []string) (bool, error) {
-		c1 := exec.Command("apk", append([]string{"search", "--exact"}, pkgs...)...)
-		c2 := exec.Command("grep", append([]string{"-q"}, pkgs...)...)
+		// c1 := exec.Command("apk", append([]string{"search", "--exact"}, pkgs...)...)
+		// c2 := exec.Command("grep", append([]string{"-q"}, pkgs...)...)
 
-		c2.Stdin, _ = c1.StdoutPipe()
-		c2.Stdout = os.Stdout
+		// c2.Stdin, _ = c1.StdoutPipe()
+		// c2.Stdout = os.Stdout
 
-		if err := c2.Start(); err != nil {
-			fmt.Println("grep start failed:", err)
-			return false, err
-		}
-		if err := c1.Run(); err != nil {
-			return false, nil // err
-		}
-		if err := c2.Wait(); err != nil {
-			return false, nil // err
-		}
-		return true, nil
+		// if err := c2.Start(); err != nil {
+		// 	fmt.Println("grep start failed:", err)
+		// 	return false, err
+		// }
+		// if err := c1.Run(); err != nil {
+		// 	return false, nil // err
+		// }
+		// if err := c2.Wait(); err != nil {
+		// 	return false, nil // err
+		// }
+		// return true, nil
+
+		opts := []string{"info", "--installed"}
+		opts = append(opts, pkgs...)
+		cmd := exec.Command(m.Bin, opts...)
+		err := cmd.Run()
+		return err == nil, nil
 	},
 }
