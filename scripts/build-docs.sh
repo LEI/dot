e2222835fe1674a46d7ccb36d5d8780eb0a63c68
Guillaume <4112243+LEI@users.noreply.github.com>
2018-08-28 02:01:29 +0200

    read

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -6,12 +6,8 @@ mkdir docs
 
 cp README.md docs/index.md
 
-# make install && "$GOPATH/bin/dot" doc --markdown docs
-
 make docs
 
 # Visualizing dependencies
 # https://golang.github.io/dep/docs/daily-dep.html#visualizing-dependencies
-if hash /usr/bin/dot 2>/dev/null; then
-  dep status -dot | /usr/bin/dot -T png > docs/deps.png
-fi
+dep status -dot | /usr/bin/dot -T png > docs/deps.png
