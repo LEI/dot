6199dcb060d4e987b06671ca25bce6e430b62a43
Guillaume <4112243+LEI@users.noreply.github.com>
2018-09-08 13:23:11 +0200

    get_url

---
+++ internal/dot/get_url.go
@@ -0,0 +1,47 @@
+package dot
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+// https://docs.ansible.com/ansible/latest/modules/get_url_module.html
+// https://github.com/LEI/dot-php/blob/master/install-composer.sh
+
+// Download from a given URL
+func getURL(url, dst string, perm os.FileMode) error {
+	if dst == "" {
+		tokens := strings.Split(url, "/")
+		dst = tokens[len(tokens)-1]
+	}
+
+	fi, err := os.Stat(dst)
+	if err != nil && os.IsExist(err) {
+		return err
+	}
+	if fi != nil && !os.IsNotExist(err) {
+		return nil // fmt.Errorf("already exists: %s", dst)
+	}
+	fmt.Printf("Downloading %s to %s...\n", url, dst)
+	output, err := os.OpenFile(dst, os.O_RDWR|os.O_CREATE|os.O_TRUNC, perm)
+	if err != nil {
+		return fmt.Errorf("error while creating %s: %s", dst, err)
+	}
+	defer output.Close()
+
+	response, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("error while downloading %s: %s", url, err)
+	}
+	defer response.Body.Close()
+
+	n, err := io.Copy(output, response.Body)
+	if err != nil {
+		return fmt.Errorf("error while copying to %s: %s", dst, err)
+	}
+	fmt.Printf("%d bytes downloaded.\n", n)
+	return nil
+}
